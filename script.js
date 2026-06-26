930ddf7742f56cf6c9d38b05a3ae674c3319642d
Abdullah Rashid <229150035+abdullahrashid359@users.noreply.github.com>
2026-06-26 22:19:51 +0500

    Add DOM UI to the project, styling is pending

--- script.js
+++ script.js
@@ -11,48 +11,66 @@ function getComputerChoice() {
 
     return choice;
 }
-
-function getHumanChoice() {
-    return prompt("Enter your choice (rock/paper/scissors): ", "");
-}
-
-function playGame() {
     let computerScore = 0;
     let humanScore = 0;
 
-    function playRound(humanChoice, computerChoice) {
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach((button) =>
+        button.addEventListener("click", playRound));
+
+    const result = document.querySelector(".result");
+    const runningScore = document.createElement("p");
+
+    runningScore.textContent = `You ${humanScore}-${computerScore} Computer`;
+    result.appendChild(runningScore);
+
+    function playRound(event) {
+        let humanChoice = event.target.textContent;
         humanChoice = humanChoice.toLowerCase();
+        const computerChoice = getComputerChoice();
+        
+        const para = document.createElement("p");
+        
 
         if(humanChoice === computerChoice)
-            console.log("It's a tie!");
+            para.textContent = "It's a tie!";
         else if ((humanChoice === "rock" && computerChoice === "scissors")
             || (humanChoice === "paper" && computerChoice === "rock")
             || (humanChoice === "scissors" && computerChoice === "paper")) {
-            console.log(`You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`);
+            para.textContent = `You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`;
 
             humanScore++;
         }
         else {
-            console.log(`You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}`)
+            para.textContent = `You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}`;
 
             computerScore++;
         }
+        
+        result.appendChild(para);
+        runningScore.textContent = `You ${humanScore}-${computerScore} Computer`;
+
+        if(humanScore === 5 || computerScore === 5) {
+            buttons.forEach(button => button.remove());
+            result.textContent = "";
+
+            const heading = document.createElement("h1");
+            heading.textContent = "Final Score";
+            result.appendChild(heading);
+
+            const finalScore = document.createElement("p");
+            finalScore.textContent = `You ${humanScore}-${computerScore} Computer`;
+            result.appendChild(finalScore);
+
+            const winner = document.createElement("p");
+            if (humanScore === 5)
+                winner.textContent = "YAYY! You da real winner.";
+            else
+                winner.textContent = "Son! You have lost.";
+
+            result.appendChild(winner);
+        }
     }
 
-    for(let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
+    
 
-        playRound(humanSelection, computerSelection);
-    }
-
-    if(humanScore === computerScore)
-        console.log("Ah! The scores are tied.");
-    else if(humanScore > computerScore)
-        console.log("YAYY! You da real winner.");
-    else
-        console.log("Son! You have lost.");
-    console.log(`Final Scores are ${humanScore}-${computerScore}`);
-}
-
-playGame();
